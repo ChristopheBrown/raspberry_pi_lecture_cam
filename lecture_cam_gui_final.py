09f88a978db36f3769f057e37ac39c12a0a7e10e
Christophe Brown <43019188+ChristopheBrown@users.noreply.github.com>
2020-05-03 16:32:26 -0400

    Add files via upload

---
+++ lecture_cam_gui_final.py
@@ -0,0 +1,245 @@
+from __future__ import division
+from board import SCL, SDA
+from guizero import *
+from picamera import PiCamera
+from time import gmtime, strftime
+from uploader import Uploader
+import Adafruit_PCA9685
+import busio
+import time
+
+# Import the PCA9685 module
+from adafruit_pca9685 import PCA9685
+from adafruit_motor import servo
+
+i2c = busio.I2C(SCL, SDA)
+
+# PCA9685 class instance
+pca = PCA9685(i2c)
+pca.frequency = 50
+servo_pan = servo.Servo(pca.channels[0])
+servo_tilt = servo.Servo(pca.channels[1])
+
+def move_up():
+    if auto_select == 0:
+        t_axis.value = t_axis.value - 10
+
+
+def move_down():
+    if auto_select == 0:
+        t_axis.value = t_axis.value + 10
+
+
+def move_left():
+    if auto_select == 0:
+        p_axis.value = p_axis.value + 3
+
+
+def move_right():
+    if auto_select == 0:
+        p_axis.value = p_axis.value - 3
+
+
+def p_axis_pos(slider_value):
+    servo_pan.angle = int(slider_value)
+
+
+def t_axis_pos(slider_value):
+    servo_tilt.angle = int(slider_value)
+
+
+def take_picture():
+    global output, file_path
+    if file_path == "":
+        camera.stop_preview()
+        app.warn("Error", "Select File Path")
+        camera.start_preview(fullscreen=False, window=(10,-40,400,400))
+    else:
+        output = strftime(file_path + "/image%d%m%H%M.png", gmtime())
+        camera.capture(output)
+    if file_path != "" and save_cloud == 1:  
+        gmail_recipients = ['ejl2187@columbia.edu','cb3534@columbia.edu']
+        u = Uploader(output, print_emails=True)
+        u.upload()
+        u.email(gmail_recipients)
+        camera.stop_preview()
+        app.info("Cloud Status", "Successfully uploaded to cloud!")
+        camera.start_preview(fullscreen=False, window=(10,-40,400,400))
+
+
+def start_rec():
+    global output, file_path, rec
+    if file_path == "":
+        camera.stop_preview()
+        app.warn("Error", "Select File Path")
+        camera.start_preview(fullscreen=False, window=(10,-40,400,400))
+    if file_path != "" and save_local == 1:   
+        rec = 1
+        rec_dot.value = "*REC"
+        output = strftime(file_path + "/lecture%d%m%H%M.h264", gmtime())
+        camera.start_recording(output)
+
+
+def stop_rec():
+    global rec
+    rec = 0
+    rec_dot.value = ""
+    timer.value = 0
+    camera.stop_recording()
+    if file_path != "" and save_cloud == 1:  
+        gmail_recipients = ['ejl2187@columbia.edu','cb3534@columbia.edu']
+        u = Uploader(output, print_emails=True)
+        u.upload()
+        u.email(gmail_recipients)
+        camera.stop_preview()
+        app.info("Cloud Status", "Successfully uploaded to cloud!")
+        camera.start_preview(fullscreen=False, window=(10,-40,400,400))
+
+
+def set_servo_pulse(channel, pulse):
+    pulse_length = 1000000 
+    pulse_length //= 60 
+    print('{0}us per period'.format(pulse_length))
+    pulse_length //= 4096
+    print('{0}us per bit'.format(pulse_length))
+    pulse *= 1000
+    pulse //= pulse_length
+    pwm.set_pwm(channel, 0, pulse)
+
+
+def get_folder():
+    global file_path
+    camera.stop_preview()
+    path.value = app.select_folder()
+    file_path = str(path.value)
+    camera.start_preview(fullscreen=False, window=(10,-40,400,400))
+
+
+def counter():
+    global rec
+    if rec == 1: 
+        timer.value = int(timer.value) + 1
+    if int(timer.value) >= duration.value:
+        stop_rec()
+
+
+def toggle_local(selected_value): 
+    global save_local
+
+    if selected_value == " ON":
+        save_local = 1
+    else:
+        save_local = 0
+
+
+def toggle_cloud(selected_value): 
+    global save_cloud
+
+    if selected_value == " ON":
+        save_cloud = 1
+    else:
+        save_cloud = 0
+
+
+def close_window():
+    camera.stop_preview()
+    i2c.deinit()
+    app.destroy()
+
+
+# Set up camera
+camera = PiCamera()
+camera.resolution = (512, 384)
+camera.hflip = True
+
+# Start camera preview
+camera.start_preview(fullscreen=False, window=(10,-40,400,400))
+
+# Set up filename
+output = ""
+file_path = ""
+B = False
+rec = 0
+save_local = 0
+save_cloud = 0
+auto_select = 0
+
+# -------------------------- Window Setup ------------------------------
+app = App(title="Lecture Cam GUI", height=800, width=480)
+app.tk.attributes("-fullscreen", True)
+
+# top box
+title_box = Box(app, width="fill", align="top")
+close_bt = PushButton(title_box, close_window, text="Exit", align="right")
+title = Text(title_box, text="Lecture Cam  ", size=30, font="Times New Roman", color="blue", align="right")
+
+# ------------------------ Recording Controls ---------------------------
+rec_box = Box(app, height="fill", align="right", border=B)
+rec_pad_box1 = Box(rec_box, height="fill", align="right", border=B)
+rec_control_box = Box(rec_box, height="fill", align="right", border=B, layout="grid")
+rec_pad_box2 = Box(rec_box, height="fill", align="right", border=B)
+
+# pad text
+# rec_pad1_text = Text(rec_pad_box1, text="  1               ")
+rec_pad2_text = Text(rec_pad_box2, text="         ")
+
+# recording controls
+pad_rec1 = Text(rec_control_box, "                                              ", grid=[1, 0])
+save_text = Text(rec_control_box, "Save Locally:", grid=[0, 1])
+save_local_tog = Combo(rec_control_box, options=["OFF", " ON"], command=toggle_local, grid=[1, 1])
+pad_rec2 = Text(rec_control_box, " ", grid=[0, 2])
+
+save_text = Text(rec_control_box, "Screenshot:", grid=[0, 3])
+screenshot = PushButton(rec_control_box, take_picture, text="Capture", grid=[1, 3])
+
+folder_bt = PushButton(rec_control_box, command=get_folder, text="Select path", padx=10, pady=2, grid=[0, 4])
+path = Text(rec_control_box, grid=[1, 4])
+pad_rec3 = Text(rec_control_box, " ", grid=[0, 5])
+
+rec_text = Text(rec_control_box, "Save to Cloud:", grid=[0, 6])
+save_cloud_tog = Combo(rec_control_box, options=["OFF", " ON"], command=toggle_cloud, grid=[1, 6])
+pad_rec4 = Text(rec_control_box, " ", grid=[0, 7])
+
+dur_text = Text(rec_control_box, "Duration (sec):", grid=[0, 8])
+duration = Slider(rec_control_box, start=0, end=150, grid=[1, 8])
+pad_rec5 = Text(rec_control_box, " ", grid=[0, 9])
+
+start_rec_bt = PushButton(rec_control_box, start_rec, text="Start Recording", grid=[0, 10])
+timer = Text(rec_control_box, text="0", grid=[1, 10])
+pad_rec6 = Text(rec_control_box, " ", grid=[0, 11])
+
+stop_rec_bt = PushButton(rec_control_box, stop_rec, text="Stop Recording", grid=[0, 12])
+rec_dot = Text(rec_control_box,  "", color="red", grid=[1, 12])
+
+timer.repeat(1000, counter)
+
+# ------------------------- Camera Controls -----------------------------
+control_box = Box(app, width="fill", align="bottom", border=True)
+control_title = Text(control_box, text="   Manual Control                        Camera Position  ", align="top")
+
+pad_box1 = Box(control_box, height="fill", align="left", border=B)
+manual_control_box = Box(control_box, height="fill", align="left", border=B, layout="grid")
+pad_box2 = Box(control_box, height="fill", align="left", border=B)
+position_control_box = Box(control_box, height="fill", align="left", border=B)
+pad_box3 = Box(control_box, height="fill", align="left", border=B)
+auto_control_box = Box(control_box, height="fill", align="left", border=B)
+
+# pad text
+pad1_text = Text(pad_box1, text="  ")
+pad2_text = Text(pad_box2, text="              ")
+pad3_text = Text(pad_box3, text="   ")
+
+# manual control
+up_button = PushButton(manual_control_box, command=move_up, text="UP", grid=[1, 0], padx=22, pady=10)
+down_button = PushButton(manual_control_box, command=move_down, text="DOWN", grid=[1, 2], padx=10, pady=10)
+left_button = PushButton(manual_control_box, command=move_left, text="LEFT", grid=[0, 1], padx=16, pady=10)
+right_button = PushButton(manual_control_box, command=move_right, text="RIGHT", grid=[2, 1], padx=10, pady=10)
+
+# position control
+p_text = Text(position_control_box, text="Pan")
+p_axis = Slider(position_control_box, command=p_axis_pos, start=0, end=180)
+p_text = Text(position_control_box, text="Tilt")
+t_axis = Slider(position_control_box, command=t_axis_pos, start=40, end=170, horizontal=True)
+
+
+app.display()
---
+++ uploader.py
@@ -0,0 +1,114 @@
+import smtplib
+import datetime
+import os
+from os import system as sys
+from email.mime.text import MIMEText
+
+
+class Uploader:
+    relative_path = None
+    file = None
+    absolute_path = None
+    keyfile = 'inteledison.pem'  # NOTE: If you get "permissions are too open" do "chmod 400 [pem file]" in terminal
+    destination_path = '/home/ec2-user/'
+    server = 'ec2-user@54.91.114.40:' + destination_path
+    gmail_sender = 'cbelinteledison@gmail.com'
+    gmail_password = 'inteledison'
+    gmail_recipients = []
+
+    show_recipients = False
+
+    def upload(self):
+        if self.file is None:
+            return
+
+        # these command performs the file copy to the remote server *COPY-SEND-DELETE*
+        command = 'cp {} {}'.format(self.absolute_path, self.file)
+        print('Copying file to local directory.')
+        print(command)
+        sys(command)
+
+        command = 'scp -i "{}" "{}" "{}"'.format(self.keyfile, self.file, self.server)
+        print('Sending to EC2 server.')
+        print(command)
+        sys(command)
+
+        command = 'rm {}'.format(self.file)
+        print('Cleaning up.')
+        print(command)
+        sys(command)
+
+        print('File {} has been uploaded to EC2 instance.'.format(self.file))
+
+    def email(self, gmail_recipients=[]):
+        if self.file is None:
+            return
+
+        for recipient in gmail_recipients:
+            self.gmail_recipients.append(recipient)
+
+        # set up email server
+        smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
+        smtpserver.ehlo()
+        smtpserver.starttls()
+        smtpserver.ehlo
+        smtpserver.login(self.gmail_sender, self.gmail_password)
+        today = datetime.date.today()
+
+        # prepare email message body
+        mail_body = 'Your file can be accessed at {}{}'.format(self.destination_path, self.file)
+        msg = MIMEText(mail_body)
+        msg['Subject'] = 'File ' + self.file + ' upload success %s' % today.strftime('%b %d %Y')
+        msg['From'] = self.gmail_sender
+
+        # send the success message to all recipients
+        for recipient in self.gmail_recipients:
+            msg['To'] = recipient
+            smtpserver.sendmail(self.gmail_sender, [recipient], msg.as_string())
+
+        smtpserver.quit()
+
+        if self.show_recipients:
+            print('The following recipients have been notified:'.format(self.file))
+            for address in self.gmail_recipients:
+                print(address)
+
+    def __init__(self, filename, print_emails=False):
+
+        # make sure PEM file and upload file exist
+        try:
+            f = open(self.keyfile)
+        except IOError:
+            print('PEM file not found. Please locate ', self.keyfile)
+        finally:
+            f.close()
+
+        try:
+            f = open(filename)
+            self.relative_path = os.path.split(filename)[0]
+            self.file = os.path.split(filename)[1]
+            self.absolute_path = str(self.relative_path) + '/' + str(self.file)
+            print(self.relative_path)
+            print(self.file)
+            print(self.absolute_path)
+
+        except IOError:
+            print('Upload file "' + filename + '" not found. Please locate it and make sure it is spelled correctly.')
+        finally:
+            f.close()
+
+        if print_emails:
+            self.show_recipients = True
+
+
+# the code below can be used to run the program by itself, no importing needed. i.e. like so in your terminal:
+# python uploader.py
+# change file_to_send below to the file you want to upload, the code will check whether your file exists
+
+if __name__ == '__main__':
+    file_to_send = 'sample.txt'
+    gmail_recipients = ['ejl2187@columbia.edu','cb3534@columbia.edu']
+
+    u = Uploader(file_to_send, print_emails=True)  # change to False to not print out emails when finished
+    u.upload()
+    u.email(gmail_recipients)
